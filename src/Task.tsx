396da0f3814adc82224889978a99a132bb0a06cf
Archhhi <brabus-20@mail.ru>
2021-05-17 11:43:34 +0300

    Added component Task

--- src/Task.tsx
+++ src/Task.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {ChangeEvent, useCallback} from 'react';
+import {Checkbox, IconButton} from "@material-ui/core";
+import {EditableSpan} from "./EditableSpan";
+import {Delete} from "@material-ui/icons";
+import {TaskType} from "./Todolist";
 
 type TaskPropsType = {
-    value: string
-    onChange: (newValue: string) => void
+  task: TaskType
+  changeTaskStatus: (taskId: string, newIsDoneValue: boolean) => void
+  removeTask: (taskId: string) => void
+  changeTaskTitle: (taskId: string, newValue: string) => void
 }
 
-export const Task = React.memo((props: TaskPropsType) => {
-    return (
-      <></>
-    )
+export const Task = React.memo((
+  {
+    task,
+    changeTaskStatus,
+    changeTaskTitle,
+    removeTask
+  }: TaskPropsType) => {
+
+  const onClickHandler = useCallback(() => removeTask(task.id), [removeTask])
+  const onChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    let newIsDoneValue = e.currentTarget.checked;
+    changeTaskStatus(task.id, newIsDoneValue);
+  }, [changeTaskStatus])
+  const onTitleChangeHandler = useCallback((newValue: string) => {
+    changeTaskTitle(task.id, newValue);
+  }, [changeTaskTitle])
+
+  return (
+    <div key={task.id} className={task.isDone ? "is-done" : ""}>
+      <Checkbox
+        checked={task.isDone}
+        color="primary"
+        onChange={onChangeHandler}
+      />
+
+      <EditableSpan value={task.title} onChange={onTitleChangeHandler}/>
+      <IconButton onClick={onClickHandler}>
+        <Delete/>
+      </IconButton>
+    </div>
+  )
 })
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -4,6 +4,7 @@ import {AddItemForm} from './AddItemForm';
 import {EditableSpan} from './EditableSpan';
 import {Button, Checkbox, IconButton} from '@material-ui/core';
 import {Delete} from '@material-ui/icons';
+import {Task} from "./Task";
 
 export type TaskType = {
   id: string
@@ -52,6 +53,14 @@ export const Todolist = React.memo((props: PropsType) => {
     tasksForTodolist = allTodolistTasks.filter(t => t.isDone === true);
   }
 
+  const removeTask = useCallback((taskId: string) => props.removeTask(taskId, props.id), [props.removeTask, props.id])
+  const changeTaskStatus = useCallback((taskId: string, newIsDoneValue: boolean) => {
+    props.changeTaskStatus(taskId, newIsDoneValue, props.id);
+  }, [props.changeTaskStatus, props.id])
+  const changeTaskTitle = useCallback((taskId: string, newValue: string) => {
+    props.changeTaskTitle(taskId, newValue, props.id);
+  }, [props.changeTaskTitle, props.id])
+
   return <div>
     <h3><EditableSpan value={props.title} onChange={changeTodolistTitle}/>
       <IconButton onClick={removeTodolist}>
@@ -61,29 +70,13 @@ export const Todolist = React.memo((props: PropsType) => {
     <AddItemForm addItem={addTask}/>
     <div>
       {
-        tasksForTodolist.map(t => {
-          const onClickHandler = () => props.removeTask(t.id, props.id)
-          const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-            let newIsDoneValue = e.currentTarget.checked;
-            props.changeTaskStatus(t.id, newIsDoneValue, props.id);
-          }
-          const onTitleChangeHandler = (newValue: string) => {
-            props.changeTaskTitle(t.id, newValue, props.id);
-          }
-
-          return <div key={t.id} className={t.isDone ? "is-done" : ""}>
-            <Checkbox
-              checked={t.isDone}
-              color="primary"
-              onChange={onChangeHandler}
-            />
-
-            <EditableSpan value={t.title} onChange={onTitleChangeHandler}/>
-            <IconButton onClick={onClickHandler}>
-              <Delete/>
-            </IconButton>
-          </div>
-        })
+        tasksForTodolist.map(t => <Task
+          key={t.id}
+          task={t}
+          changeTaskStatus={changeTaskStatus}
+          removeTask={removeTask}
+          changeTaskTitle={changeTaskTitle}
+        />)
       }
     </div>
     <div>
